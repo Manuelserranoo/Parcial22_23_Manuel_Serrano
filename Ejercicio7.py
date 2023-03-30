e99cab1c50ed8bc039f02b0a934b8a4b92867d23
Manoloserranoo <manuel.serranovallejo@usp.ceu.es>
2023-03-30 08:55:46 +0200

    cambios

---
+++ Ejercicio7.py
@@ -0,0 +1,6 @@
+class polinomio:
+    def __init__(self,coeficientes):
+        self.coeficientes=coeficientes
+        print("El polinomio se ha creado con éxito")
+    def __sum__(self,otro):
+        if len
