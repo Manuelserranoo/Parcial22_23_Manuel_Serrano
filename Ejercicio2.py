3dfcbe487279bfe13144bb5eab0e3819a68cf5d8
Manoloserranoo <manuel.serranovallejo@usp.ceu.es>
2023-03-30 09:17:01 +0200

    cambios

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -6,9 +6,43 @@ def comprobar(lista):
             break
     return lista
 
+def merge_sort(lista):
+    if len(lista) > 1:
+        mitad = len(lista) // 2
+        izquierda = lista[:mitad]
+        derecha = lista[mitad:]
+
+        merge_sort(izquierda)
+        merge_sort(derecha)
+
+        i = j = k = 0
+
+        while i < len(izquierda) and j < len(derecha):
+            if izquierda[i] < derecha[j]:
+                lista[k] = izquierda[i]
+                i += 1
+            else:
+                lista[k] = derecha[j]
+                j += 1
+            k += 1
+
+        while i < len(izquierda):
+            lista[k] = izquierda[i]
+            i += 1
+            k += 1
+
+        while j < len(derecha):
+            lista[k] = derecha[j]
+            j += 1
+            k += 1
+    return lista
+
 def main():
     lista = [18, 50, 210, 80, 145, 333, 70, 30]
     print(comprobar(lista))
+    print(merge_sort(lista))
+    
+
 
 
 if __name__ == "__main__":
